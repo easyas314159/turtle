0c0a405dcf553538c6717e99ad549c6c73ef6dee
Kevin Loney <kevin.loney@gmail.com>
2020-05-24 15:44:48 -0600

    Added clean server shutdown handling

--- server/__main__.py
+++ server/__main__.py
@@ -1,6 +1,12 @@
+import sys
+import signal
+import logging
 import argparse
 
-from tornado import ioloop
+from functools import partial
+from contextlib import ExitStack
+
+from tornado import ioloop, locks
 
 def get_cli_arguments():
 	parser = argparse.ArgumentParser()
@@ -9,6 +15,21 @@ def get_cli_arguments():
 
 async def main():
 	args = get_cli_arguments()
+
+	stopped = locks.Event()
+	shutdown_handler = partial(exit_handler, callback=lambda: stopped.set())
+
+	signal.signal(signal.SIGINT, shutdown_handler)
+	signal.signal(signal.SIGTERM, shutdown_handler) # Include SIGTERM so we play nice with docker
+
+	with ExitStack() as stack:
+		# Wait for shutdown
+		await stopped.wait()
+		logging.info('Starting shutdown')
+
+def exit_handler(sig, frame, callback=None):
+	if callback:
+		ioloop.IOLoop.current().add_callback_from_signal(callback)
 
 if __name__ == '__main__':
 	ioloop.IOLoop.current().run_sync(main)
