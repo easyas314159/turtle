b0864b9d1f5d2ab7a361be8231bb70730eacfd11
Kevin Loney <kevin.loney@gmail.com>
2020-05-24 16:59:42 -0600

    Started adding basic static resource endpoints to the web application

--- server/app.py
+++ server/app.py
@@ -1,4 +1,20 @@
+import os
+import logging
+
 from tornado import web
 
 def create_application(args):
-	return web.Application()
+	static_resources = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+
+	logging.debug('Serving static resources from: %s', static_resources)
+	logging.debug('Serving rendered results from: %s', args.tmp_dir)
+
+	return web.Application(
+		[
+			('/', web.RedirectHandler, {'url': '/index.html'}),
+			(r'/turtle/(.*\.svg)', web.StaticFileHandler, {'path': args.tmp_dir}),
+			(r'/(.*)', web.StaticFileHandler, {'path': static_resources}),
+		],
+		compress_response=True,
+		debug=args.debug,
+	)
