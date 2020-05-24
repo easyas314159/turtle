2d8f0a7e86337dae499cc7ec6357309a7db3e95a
Kevin Loney <kevin.loney@gmail.com>
2020-05-24 16:24:17 -0600

    Added logging configuration

--- server/__main__.py
+++ server/__main__.py
@@ -16,6 +16,7 @@ def get_cli_arguments():
 	parser = argparse.ArgumentParser()
 
 	parser.add_argument('--tmp-dir', default=None, help='')
+	parser.add_argument('--debug', default=False, action='store_true')
 
 	parser.add_argument('-I', '--if', dest='interface', default='127.0.0.1', help='')
 	parser.add_argument('-p', '--port', type=int, default=8080, help='')
@@ -24,6 +25,9 @@ def get_cli_arguments():
 
 async def main():
 	args = get_cli_arguments()
+
+	# Configure logging
+	logging.getLogger().setLevel(logging.DEBUG if args.debug else logging.INFO)
 
 	stopped = locks.Event()
 	shutdown_handler = partial(exit_handler, callback=lambda: stopped.set())
@@ -61,4 +65,14 @@ def exit_handler(sig, frame, callback=None):
 		ioloop.IOLoop.current().add_callback_from_signal(callback)
 
 if __name__ == '__main__':
+	# Logging config needs to happen before the tornado ioloop
+	# starts or tornado will hijack the logging config
+	# See: https://github.com/tornadoweb/tornado/blob/stable/tornado/ioloop.py#L445
+	logging.basicConfig(
+		stream=sys.stderr,
+		format='%(asctime)s %(name)s %(levelname)s - %(message)s',
+	)
+	logging.getLogger('py.warnings').setLevel(logging.ERROR)
+	logging.captureWarnings(True)
+
 	ioloop.IOLoop.current().run_sync(main)
