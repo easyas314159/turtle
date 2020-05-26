34124c9411eeb170f1ffa417edfeabe16967710e
Kevin Loney <kevin.loney@gmail.com>
2020-05-26 15:25:29 -0600

    Added the basic redux store to the react app

--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 import './scss/custom.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {Provider} from 'react-redux';
+
+import store from './store.js';
+
+ReactDOM.render(
+	<Provider store={store}>
+	</Provider>,
+	document.getElementById('app')
+);
