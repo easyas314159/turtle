b91f0fcac0130bb3b21784b7fca7a3d61334cb54
Kevin Loney <kevin.loney@gmail.com>
2020-05-26 21:20:17 -0600

    Added `Display` parent component

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 import Navigation from './Navigation.js';
-import Beach from './Beach.js';
+import Display from './Display.js';
 
 class App extends React.Component {
 	render() {
 		return (
 			<>
 				<Navigation />
-				<Beach />
+				<Display />
 			</>
 		);
 	}
